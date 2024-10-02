67c9f75a727baa85a877f43839d6bc9533d30430
mike <laniermike17@gmail.com>
2024-10-02 12:40:07 -0400

    Editing model for player stats

--- dbt_nfl/dev_tables.sql
+++ dbt_nfl/dev_tables.sql
@@ -53,9 +53,45 @@ insert into landing.conferences values
 (1, 'American Football Conference', 'American', 'AFC'),
 (2, 'National Football Conference', 'National', 'NFC');
 
-create table landing.player_stats (
-    id SERIAL PRIMARY KEY,
-    stats_json JSONB,
-    filename VARCHAR(30),
-    etl_ts TIMESTAMP
+
+create table landing.pro_fb_ref_team_match as (
+    select
+    team_id
+    , case
+        when team_id = 1 then 'ATL'
+        when team_id = 2 then 'BUF'
+        when team_id = 3 then 'CHI'
+        when team_id = 4 then 'CIN'
+        when team_id = 5 then 'CLE'
+        when team_id = 6 then 'DAL'
+        when team_id = 7 then 'DEN'
+        when team_id = 8 then 'DET'
+        when team_id = 9 then 'GNB'
+        when team_id = 10 then 'TEN'
+        when team_id = 11 then 'IND'
+        when team_id = 12 then 'KAN'
+        when team_id = 13 then 'LVR'
+        when team_id = 14 then 'LAR'
+        when team_id = 15 then 'MIA'
+        when team_id = 16 then 'MIN'
+        when team_id = 17 then 'NWE'
+        when team_id = 18 then 'NOR'
+        when team_id = 19 then 'NYG'
+        when team_id = 20 then 'NYJ'
+        when team_id = 21 then 'PHI'
+        when team_id = 22 then 'ARI'
+        when team_id = 23 then 'PIT'
+        when team_id = 24 then 'LAC'
+        when team_id = 25 then 'SFO'
+        when team_id = 26 then 'SEA'
+        when team_id = 27 then 'TAM'
+        when team_id = 28 then 'WAS'
+        when team_id = 29 then 'CAR'
+        when team_id = 30 then 'JAX'
+        when team_id = 33 then 'BAL'
+        when team_id = 34 then 'HOU'
+        else null
+      end as pfr_team
+    from
+    prod.teams t
 );
--- dbt_nfl/models/player_stats.sql
+++ dbt_nfl/models/player_stats.sql
@@ -1,28 +1,64 @@
-{{
-    config(
-        materialized='incremental'
-    )
-}}
-
-
 with tmp as (
     select
-    
-    , split_part(filename, '.json', 1) as game_id
-    , etl_ts
+    ps.player_name
+    , t.team_id
+    , ps.position
+    , ps.age
+    , ps.games_played
+    , ps.games_started
+    , ps.pass_comp
+    , ps.pass_att
+    , ps.pass_yds
+    , ps.pass_tds
+    , ps.pass_int
+    , ps.rush_att
+    , ps.rush_yds
+    , ps.rush_yds_per_att
+    , ps.rush_tds
+    , ps.rec_tgt
+    , ps.receptions
+    , ps.rec_yds
+    , ps.rec_yds_per_catch
+    , ps.rec_tds
+    , ps.fumbles
+    , ps.fumbles_lost
+    , ps.total_tds
+    , ps.fantasy_ppr
     from
-    landing.plays
+    landing.player_stats ps
+    join
+    landing.pro_fb_ref_team_match t
+    on
+    ps.team = t.pfr_team
+    where
+    ps.player_rank != 'Rk'
 )
 
 select
-
-, game_id::int
+player_name
+, team_id::int
+, position
+, age::int
+, games_played::int
+, games_started::int
+, pass_comp::int
+, pass_att::int
+, pass_yds::int
+, pass_tds::int
+, pass_int::int
+, rush_att::int
+, rush_yds::int
+, rush_yds_per_att::real
+, rush_tds::int
+, rec_tgt::int
+, receptions::int
+, rec_yds::int
+, rec_yds_per_catch::real
+, rec_tds::int
+, fumbles::int
+, fumbles_lost::int
+, total_tds::int
+, fantasy_ppr::real
 , current_timestamp::timestamp as elt_ts
 from
 tmp
-
-{% if is_incremental() %}
-
-where etl_ts >= (select max(elt_ts) from {{ this }})
-
-{% endif %}
